6e59a6cfa9b95b97cab5e3ee1d276ceff4f91f54
Chris de Klerk <cdeklerk@singular.co.za>
2024-10-08 18:28:36 +0200

    Documentation Update.

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,6 +1,6 @@
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity"
-import { SyntheticEvent, useState } from "react";
+import { useState } from "react";
 
 interface IActivityListProps {
     activities: Activity[];
@@ -11,12 +11,15 @@ interface IActivityListProps {
 
 // CDK20241006 - divided just adds the nice division between the groups. Remember to add a key when you are looping.
 export default function ActivityList({ activities, selectActivity, deleteActivity, submitting }: IActivityListProps) {
-const [target, setTarget] = useState('');
+    const [target, setTarget] = useState('');
 
-function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-    setTarget(e.currentTarget.name);
-    deleteActivity(id);
-}
+    // CDK20241007 - This is a great way of targetting individual UI elements. We setup the above state for the button - to give the button a name.
+    // CDK20241007 - We create this function to take in the MouseEvent of the button click    
+    function handleActivityDelete(e: React.MouseEvent<HTMLButtonElement>, id: string) {
+        // CDK20241007 - We then set our target state to the name of the button (which we set to activity.id) and then we delete. So only that button is impacted.
+        setTarget(e.currentTarget.name);
+        deleteActivity(id);
+    }
 
     return (
         <Segment>
